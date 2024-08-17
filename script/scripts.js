4af705cb8c342e39fb81d3c112fcd9cb28dbb783
Rodionjke <Rodionjke@gmail.com>
2024-08-17 04:21:00 +0300

    Added preprocessor, Added adaptive. Deleted styles.css and added style.scss, deleted media.css

--- script/scripts.js
+++ script/scripts.js
@@ -39,6 +39,15 @@ $('.owl-carousel').owlCarousel({
         0:{
             items:1
         },
+        400:{
+            items:1
+        },
+        800:{
+            items:2
+        },
+        1000:{
+            items:2
+        },
         1100:{
             items:4
         }
